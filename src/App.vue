0611d791dcd38645044a66341d7a8223e6806b69
tasyamlyni <tasyamulyani@student.uir.ac.id>
2025-05-06 12:11:59 +0700

    add task

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,26 @@ const tasks = ref([
   { id: 3, description: 'Task 3', completed: false },
 ]);
 
+const newTask = ref('');
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push({
+      id: tasks.value.length + 1,
+      description: newTask.value,
+      completed: false,
+    });
+    newTask.value = '';
+  }
+};
+
 </script>
 
 <template>
   <div>
+    <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+    <button @click="addTask">Add Task</button>
+    
     <ul>
       <li v-for="task in tasks" :key="task.id">
         {{  task.description }} - {{ task.completed ? 'Completed' : 'Not Completed' }}
