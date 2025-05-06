02b199dc6b6ec8ea9d1677213b6c1bd6168355c1
tasyamlyni <tasyamulyani@student.uir.ac.id>
2025-05-06 12:10:27 +0700

    display task

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+
+const tasks = ref([
+  { id: 1, description: 'Task 1', completed: false },
+  { id: 2, description: 'Task 2', completed: true },
+  { id: 3, description: 'Task 3', completed: false },
+]);
+
 </script>
 
 <template>
   <div>
-    
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{  task.description }} - {{ task.completed ? 'Completed' : 'Not Completed' }}
+      </li>
+    </ul>
   </div>
 </template>
 
